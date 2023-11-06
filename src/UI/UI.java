08c539ef78cb3ff780cea94f85e9d85f2425f0dc
bryan robbins <btr1014@usnh.edu>
2023-11-06 16:13:33 -0500

    basic JLayer AdvancedPlayer playback

--- src/UI/UI.java
+++ src/UI/UI.java
@@ -1,66 +1,121 @@
 package UI;
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.BorderLayout;
-import javax.swing.JLabel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JFileChooser;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.DirectoryChooser;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
-public class UI {
+public class UI extends Application {
 
-	private JFrame frame;
-	private String selectedDirectory;  // Variable to store the selected directory
+    private String selectedDirectory;
+    private MediaPlayer mediaPlayer;
+    private boolean isPlaying = false;
+    private boolean isPaused = false;
+    private Label lblNewLabel;
+    private Button playButton;
+    private int pausePosition = 0; // Track the pause position
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					UI window = new UI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	/**
-	 * Create the application.
-	 */
-	public UI() {
-		initialize();
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("MP3 Player");
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select a directory");
+        File directory = directoryChooser.showDialog(primaryStage);
+        if (directory != null) {
+            selectedDirectory = directory.getAbsolutePath();
+        }
 
-		// Open directory selection dialog
-		JFileChooser fileChooser = new JFileChooser();
-		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);  // Allow only directories to be selected
-		int returnValue = fileChooser.showOpenDialog(frame);
-		if (returnValue == JFileChooser.APPROVE_OPTION) {
-			selectedDirectory = fileChooser.getSelectedFile().getAbsolutePath();  // Save selected directory to variable
-		}
+        playButton = new Button("Play");
+        playButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                if (!isPlaying) {
+                    if (!isPaused) {
+                        playFirstSong(selectedDirectory, 0); // Pass 0 as the initial frame position
+                    } else {
+                        // Resume from the pause position
+                        resumeMusic(pausePosition);
+                    }
+                } else {
+                    pauseMusic();
+                }
+            }
+        });
 
-		JButton songLabel = new JButton("Play/Pause");
-		songLabel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		frame.getContentPane().add(songLabel, BorderLayout.CENTER);
+        lblNewLabel = new Label("Current Song: ");
+        BorderPane root = new BorderPane();
+        root.setCenter(playButton);
+        root.setTop(lblNewLabel);
 
-		JLabel lblNewLabel = new JLabel("Current Song:");
-		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
-	}
+        Scene scene = new Scene(root, 450, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void playFirstSong(String directory, int framePosition) {
+        File dir = new File(directory);
+        File[] files = dir.listFiles();
+
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                if (file.getName().toLowerCase().endsWith(".mp3")) {
+                    Media media = new Media(file.toURI().toString());
+                    mediaPlayer = new MediaPlayer(media);
+
+                    mediaPlayer.setOnEndOfMedia(() -> {
+                        // Capture the current frame position
+                        pausePosition = (int) mediaPlayer.getCurrentTime().toSeconds();
+                        System.out.println(pausePosition);
+                    });
+
+                    isPlaying = true;
+                    isPaused = false;
+                    updateSongLabel(file.getName());
+                    mediaPlayer.play();
+                    playButton.setText("Pause");
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private void pauseMusic() {
+        if (isPlaying && mediaPlayer != null) {
+            mediaPlayer.pause();
+            isPlaying = false;
+            isPaused = true;
+            playButton.setText("Resume");
+        }
+    }
+
+    private void resumeMusic(int framePosition) {
+        if (isPaused) {
+            isPaused = false;
+            playButton.setText("Pause");
+            playFirstSong(selectedDirectory, framePosition);
+        }
+    }
+
+    private void updateSongLabel(String songName) {
+        lblNewLabel.setText("Current Song: " + songName);
+    }
+
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
 }
