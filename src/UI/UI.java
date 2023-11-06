b60149884a25c78dd91985b8cd5e983b2b638d0f
bryan robbins <btr1014@usnh.edu>
2023-11-06 18:27:05 -0500

    JavaFX

--- src/UI/UI.java
+++ src/UI/UI.java
@@ -1,121 +1,161 @@
 package UI;
-
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.stage.DirectoryChooser;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.DirectoryChooser;
+import javafx.util.Duration;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-public class UI extends Application {
-
-    private String selectedDirectory;
+public class UI extends JFrame {
+    private final JButton playButton;
+    private final JButton nextButton;
+    private final JButton prevButton;
+    private final JLabel songLabel;
+    private final JSlider scrubBar;
+    private final JLabel currentTimeLabel;
+    private final JLabel totalTimeLabel;
     private MediaPlayer mediaPlayer;
-    private boolean isPlaying = false;
-    private boolean isPaused = false;
-    private Label lblNewLabel;
-    private Button playButton;
-    private int pausePosition = 0; // Track the pause position
+    private List<File> songFiles;
+    private int currentSongIndex = -1;
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("mm:ss");
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public UI() {
+        // Initialize the JavaFX environment
+        new JFXPanel();
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("MP3 Player");
+        // Swing components
+        playButton = new JButton("Play");
+        nextButton = new JButton("Next");
+        prevButton = new JButton("Previous");
+        songLabel = new JLabel("No song playing", SwingConstants.CENTER);
+        scrubBar = new JSlider();
+        currentTimeLabel = new JLabel("--:--");
+        totalTimeLabel = new JLabel("--:--");
 
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        directoryChooser.setTitle("Select a directory");
-        File directory = directoryChooser.showDialog(primaryStage);
-        if (directory != null) {
-            selectedDirectory = directory.getAbsolutePath();
-        }
+        // Set up the JFrame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 200);
+        setLayout(new BorderLayout(5, 5));
 
-        playButton = new Button("Play");
-        playButton.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                if (!isPlaying) {
-                    if (!isPaused) {
-                        playFirstSong(selectedDirectory, 0); // Pass 0 as the initial frame position
-                    } else {
-                        // Resume from the pause position
-                        resumeMusic(pausePosition);
-                    }
-                } else {
-                    pauseMusic();
+        JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.add(songLabel, BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.add(playButton, BorderLayout.WEST);
+        bottomPanel.add(nextButton, BorderLayout.EAST);
+        bottomPanel.add(prevButton, BorderLayout.CENTER);
+
+        JPanel scrubPanel = new JPanel(new BorderLayout(5, 0));
+        scrubPanel.add(currentTimeLabel, BorderLayout.WEST);
+        scrubPanel.add(scrubBar, BorderLayout.CENTER);
+        scrubPanel.add(totalTimeLabel, BorderLayout.EAST);
+
+        add(topPanel, BorderLayout.NORTH);
+        add(scrubPanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        // Action listeners for buttons
+        playButton.addActionListener(this::playPause);
+        nextButton.addActionListener(e -> playNextSong());
+        prevButton.addActionListener(e -> playPreviousSong());
+
+        scrubBar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                if (mediaPlayer != null && mediaPlayer.getTotalDuration().greaterThan(Duration.ZERO)) {
+                    int mouseX = evt.getX();
+                    double scrubValue = (double) mouseX / (double) scrubBar.getWidth() * scrubBar.getMaximum();
+                    Duration seekTo = mediaPlayer.getTotalDuration().multiply(scrubValue / 100.0);
+                    mediaPlayer.seek(seekTo);
                 }
             }
         });
 
-        lblNewLabel = new Label("Current Song: ");
-        BorderPane root = new BorderPane();
-        root.setCenter(playButton);
-        root.setTop(lblNewLabel);
-
-        Scene scene = new Scene(root, 450, 300);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        // Set up the JavaFX thread
+        Platform.runLater(this::initializeMediaPlayer);
     }
 
-    private void playFirstSong(String directory, int framePosition) {
-        File dir = new File(directory);
-        File[] files = dir.listFiles();
+    private void initializeMediaPlayer() {
+        // Show directory chooser on the JavaFX Application Thread
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(null);
+        if (selectedDirectory != null) {
+            songFiles = new ArrayList<>(List.of(selectedDirectory.listFiles((dir, name) -> name.toLowerCase().endsWith(".mp3"))));
+            if (!songFiles.isEmpty()) {
+                playNextSong();
+            } else {
+                JOptionPane.showMessageDialog(this, "No MP3 files found in the selected directory.");
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "No directory selected.");
+        }
+    }
 
-        if (files != null && files.length > 0) {
-            for (File file : files) {
-                if (file.getName().toLowerCase().endsWith(".mp3")) {
-                    Media media = new Media(file.toURI().toString());
-                    mediaPlayer = new MediaPlayer(media);
-
-                    mediaPlayer.setOnEndOfMedia(() -> {
-                        // Capture the current frame position
-                        pausePosition = (int) mediaPlayer.getCurrentTime().toSeconds();
-                        System.out.println(pausePosition);
-                    });
-
-                    isPlaying = true;
-                    isPaused = false;
-                    updateSongLabel(file.getName());
-                    mediaPlayer.play();
-                    playButton.setText("Pause");
-
-                    break;
-                }
+    private void playPause(ActionEvent event) {
+        if (mediaPlayer != null) {
+            MediaPlayer.Status status = mediaPlayer.getStatus();
+            if (status == MediaPlayer.Status.PLAYING) {
+                mediaPlayer.pause();
+                playButton.setText("Play");
+            } else {
+                mediaPlayer.play();
+                playButton.setText("Pause");
             }
         }
     }
 
-    private void pauseMusic() {
-        if (isPlaying && mediaPlayer != null) {
-            mediaPlayer.pause();
-            isPlaying = false;
-            isPaused = true;
-            playButton.setText("Resume");
+    private void playNextSong() {
+        if (songFiles != null && !songFiles.isEmpty()) {
+            currentSongIndex = (currentSongIndex + 1) % songFiles.size();
+            playSong(currentSongIndex);
         }
     }
 
-    private void resumeMusic(int framePosition) {
-        if (isPaused) {
-            isPaused = false;
-            playButton.setText("Pause");
-            playFirstSong(selectedDirectory, framePosition);
+    private void playPreviousSong() {
+        if (songFiles != null && !songFiles.isEmpty()) {
+            currentSongIndex = (currentSongIndex - 1 + songFiles.size()) % songFiles.size();
+            playSong(currentSongIndex);
         }
     }
 
-    private void updateSongLabel(String songName) {
-        lblNewLabel.setText("Current Song: " + songName);
-    }
-
-    public void stop() {
+    private void playSong(int index) {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
+        Media media = new Media(songFiles.get(index).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setOnReady(() -> {
+            mediaPlayer.play();
+            songLabel.setText(songFiles.get(index).getName());
+            playButton.setText("Pause");
+            Duration totalDuration = mediaPlayer.getMedia().getDuration();
+            totalTimeLabel.setText(timeFormat.format(new Date((long) totalDuration.toMillis())));
+        });
+
+        mediaPlayer.currentTimeProperty().addListener((observable, oldValue, newValue) -> {
+            Platform.runLater(() -> updateScrubBar(newValue));
+        });
+
+        mediaPlayer.setOnEndOfMedia(this::playNextSong);
+    }
+
+    private void updateScrubBar(Duration currentTime) {
+        double progress = currentTime.toMillis() / mediaPlayer.getTotalDuration().toMillis();
+        scrubBar.setValue((int) (progress * 100));
+        currentTimeLabel.setText(timeFormat.format(new Date((long) currentTime.toMillis())));
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new UI().setVisible(true);
+        });
     }
 }
